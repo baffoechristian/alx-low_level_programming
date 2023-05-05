104e5bd2ced20e40f5f1702b85bde5eb98e7bfaf
baffoechristian <christianbaffoe444@gmail.com>
2023-05-05 16:39:39 -0700

    task 7

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - determines if a computer is large or tiny endian
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
